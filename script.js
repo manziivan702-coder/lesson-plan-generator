f144690278183055df0fe46612b75cbc3db3aba6
manziivan702-coder <manziivan702@gmail.com>
2026-05-23 13:45:44 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// fresh start
+// fresh startlet steps = document.querySelectorAll(".step");
+let buttons = document.querySelectorAll(".step-btn");
+
+function goStep(index) {
+  steps.forEach((s, i) => {
+    s.classList.remove("active");
+    if (i === index) s.classList.add("active");
+  });
+
+  buttons.forEach((b, i) => {
+    b.classList.remove("active");
+    if (i === index) b.classList.add("active");
+  });
+}
